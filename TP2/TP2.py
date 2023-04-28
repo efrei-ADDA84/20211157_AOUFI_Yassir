73da331bfc5ddd6f5683f02075da467e6ba8ab88
YASSIR <yass1338@gmail.com>
2023-04-28 16:28:19 +0200

    TP1 & TP2

---
+++ TP2/TP2.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import os
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST', 'PUT'])
+def weather():
+    lat = 48.85667
+    lon = 2.35222
+    api_key = os.environ.get('API_KEY')
+    url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}'
+    response = requests.get(url)
+    if response.status_code == 200:
+        weather_data = response.json()
+        response = {
+            'temperature': round(weather_data['main']['temp'] - 272.15, 2), 
+            'description': weather_data['weather'][0]['description']
+        }
+        return jsonify(response)
+    else:
+        return jsonify({'error': 'unable to get weather data'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8081)
